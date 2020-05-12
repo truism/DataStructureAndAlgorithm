ea98e9a8f5c529c5d4ddf0bf23604befc64594b8
Admin <liumingli95@gmail.com>
2020-05-13 00:16:36 +0800

    寻找数组中第一个重复元素 时间复杂度O(n) 空间复杂度O(1)

---
+++ DataStructure/src/stu/edu/InterviewQuestion/DuplicateNumber.java
@@ -0,0 +1,35 @@
+package stu.edu.InterviewQuestion;
+
+/**
+ * 找到数组中第一个重复的元素
+ * @author Admin
+ */
+public class DuplicateNumber {
+
+    public static void main(String[] args) {
+        int[] arr = new int[] {1,2,4,3,5,2,1,3};
+        System.out.println(duplicateNUmber(arr));
+    }
+
+    public static int duplicateNUmber(int[] arr) {
+        if(arr == null || arr.length <= 1) {
+            throw new RuntimeException("No duplicate number.");
+        }
+        for (int i = 0; i < arr.length; i++) {
+            while (arr[i] != i) {
+                if(arr[i] == arr[arr[i]]) {
+                    return arr[i];
+                }
+                // 将元素arr[i]放到第i位
+                swap(arr, i, arr[i]);
+            }
+        }
+        throw new RuntimeException("No duplicate number.");
+    }
+
+    private static void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
